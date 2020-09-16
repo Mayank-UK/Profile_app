3c494cce6dbd1262a8c434ae693e2b9a502285a2
= <mayankupadhyay191298@gmail.com>
2020-09-16 16:35:42 +0530

    docker config updated

--- tests/unit/sample_test.php
+++ tests/unit/sample_test.php
@@ -2,9 +2,10 @@
 
 use PHPUnit\Framework\TestCase;
 
+//use "test" keyword in the name of test class and it's methods
 class SampleTest extends TestCase
 {
-    public function testTrueAssertsToTrue()
+    public function test_sample()
     {
         $this->assertTrue(true);
     }
